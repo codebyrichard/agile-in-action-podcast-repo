0dd7107a613a1cb947c0c504d54b94ad3ff7a629
codebyrichard <103580523+codebyrichard@users.noreply.github.com>
2022-04-19 21:09:25 +0100

    Check difference between arg and env

--- tweet-from-json/script.py
+++ tweet-from-json/script.py
@@ -1,7 +1,7 @@
 import os
 print("HELLO WORLD!!!")
-if (os.environ.get("TWITTER_ACCESS_TOKEN")):
-  print ("YAY!!!")
-else:
-  print ("NAH!!!")
+if (os.environ.get("env_twitter_access_token")=="Loremipsum"):
+  print ("ENV WORKS!!!")
+if (os.environ.get("twitter_access_token")=="Loremipsum"):
+  print ("WITHOUT_ENV WORKS!!!")
 print(os.environ.items())
