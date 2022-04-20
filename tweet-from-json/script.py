07b5eb452d81f3141ea32fad0e4bcd6f73a465a0
codebyrichard <103580523+codebyrichard@users.noreply.github.com>
2022-04-20 11:39:15 +0100

    Update script.py

--- tweet-from-json/script.py
+++ tweet-from-json/script.py
@@ -1,6 +1,6 @@
 import os
 print("HELLO WORLD!!!\n")
-print ("TEST_VAR: " + os.environ.get("TEST_VAR"))
+print ("TEST_VAR: " + os.environ.get("test_var"))
 if (os.environ.get("env_twitter_access_token")=="Loremipsum"):
   print ("ENV WORKS!!!")
 if (os.environ.get("twitter_access_token")=="Loremipsum"):
