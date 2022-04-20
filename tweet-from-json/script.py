bb1f1a9bda61e1636144ea810dcb19324970120a
codebyrichard <103580523+codebyrichard@users.noreply.github.com>
2022-04-20 17:28:15 +0100

    test script with CMD

--- tweet-from-json/script.py
+++ tweet-from-json/script.py
@@ -1,8 +1,4 @@
 import os
 print("HELLO WORLD!!!\n")
 print ("TEST_VAR: " + os.environ.get("test_var"))
-if (os.environ.get("env_twitter_access_token")=="Loremipsum"):
-  print ("ENV WORKS!!!")
-if (os.environ.get("twitter_access_token")=="Loremipsum"):
-  print ("WITHOUT_ENV WORKS!!!")
 print(os.environ.items())
